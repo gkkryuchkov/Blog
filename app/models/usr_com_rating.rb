de3a7ff99449431e07f9d3df9b84f8a44c413962
gkkryuchkov <egorkak1999@mail.ru>
2020-03-06 20:45:32 +0300

    added comments validation

--- app/models/usr_com_rating.rb
+++ app/models/usr_com_rating.rb
@@ -3,10 +3,18 @@ class UsrComRating < ApplicationRecord
   belongs_to :comment
 
   validate :unique
+  validate :self_comment
+
+  def self_comment
+    if self.user_profile_id == Comment.find(self.comment_id).user_profile_id
+      errors[:base] << 'Only one assertion'
+    end
+  end
+
   def unique
     if UsrComRating.find_by(user_profile_id: self.user_profile_id,
                             comment_id: self.comment_id)
-      errors[:base] << 'Only one assertion'
+      errors[:base] << 'You could not rate your own comments'
     end
   end
 end
